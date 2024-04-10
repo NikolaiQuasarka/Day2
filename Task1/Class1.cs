747401f4a8c27665b9154fe7bd380d19d41a66dc
Nikolai Quasarka <nikolaiquasarka@gmail.com>
2024-04-10 21:57:19 +0300

    Обязательно

--- Task1/Class1.cs
+++ Task1/Class1.cs
@@ -11,6 +11,8 @@ namespace Task1
         public int NOD { get; set; }
         public int a { get; set; }
         public int b { get; set; }
+        public int c { get; set; }
+
 
         public static NODCl NODSearch(int m, int n)
         {
@@ -29,6 +31,13 @@ namespace Task1
                 b = tempY;
             }
             return new NODCl { NOD = m, a = a, b = b };
+        }
+        public static NODCl NODSearch(int m, int n, int p)
+        {
+            NODCl nod1 = NODSearch(m, n);
+            NODCl nod2 = NODSearch(nod1.NOD, p);
+
+            return new NODCl { NOD = nod2.NOD, a = nod1.a, b = nod1.b, c = nod2.a };
         }
     }
 }
